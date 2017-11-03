bcd159d97b1d15441bfa1489a332b0e71543fd9c
mxiyuan <mxiyuan@outlook.com>
2017-11-03 20:11:04 +0800

    define file2matrix in kNN

--- ch02/kNN.py
+++ ch02/kNN.py
@@ -6,3 +6,32 @@ def createDataSet():
     labels = ['A', 'A', 'B', 'B']
     return group, labels
 
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis = 1)
+    distances = sqDistances ** 0.5
+    sortedDistIndicis = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteILabel = labels[sortedDistIndicis[i]]
+        classCount[voteILabel] = classCount.get(voteILabel, 0) + 1
+    sortedClassCount = sorted(classCount.items(),
+    key = operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+def file2matrix(fileName):
+    fr = open(fileName)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    for line in arrayOLines
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :]= listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
