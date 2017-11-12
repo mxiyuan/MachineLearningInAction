04e53d0f404094bdb725e5d8f99f638dd402a1d4
mxiyuan <mxiyuan@outlook.com>
2017-11-12 13:19:01 +0800

    refine classify0

--- ch02/kNN.py
+++ ch02/kNN.py
@@ -17,8 +17,8 @@ def classify0(inX, dataSet, labels, k):
     for i in range(k):
         voteILabel = labels[sortedDistIndicis[i]]
         classCount[voteILabel] = classCount.get(voteILabel, 0) + 1
-    sortedClassCount = sorted(classCount.items(),
-    key = operator.itemgetter(1), reverse=True)
+    sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1),\
+                                reverse=True)
     return sortedClassCount[0][0]
 
 def file2matrix(fileName):
@@ -84,4 +84,31 @@ def img2vector(fileName):
             returnVect[0, 32 * i + j] = int(lineStr[j])
     return returnVect
 
-def 
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('traingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros(m, 1024)
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = imag2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, \
+                            trainingMat, hwLabels, 3)
+        print("the classifier came back with: %d, the real answer is : %d" \
+                % (classifierResult, classNumStr))
+        if(classifierResult != classNumStr) :
+            errorCount += 1.0
+        print("\n the total number of errors is : %d" % errorCount)
+        print("\n the total error rate is : %f" % (errorCount/float(mTest)))
+        
