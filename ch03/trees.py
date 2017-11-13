1ae81bb7d11e3208ae00e41611cccef2a27dbf4a
mxiyuan <mxiyuan@outlook.com>
2017-11-13 22:17:09 +0800

    add treePlotter.py

--- ch03/trees.py
+++ ch03/trees.py
@@ -49,3 +49,28 @@ def chooseBestFeatureToSplit(dataSet):
             bestFeature = i
     return bestFeature
 
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        classCount[vote] = classCount.get(vote, 0) + 1
+    sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), 
+                            reverse=True)
+    return sortedClassCount
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if (len(dataSet[0]) == 1):
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, 
+                                        bestFeat, value), subLabels)
+    return myTree
