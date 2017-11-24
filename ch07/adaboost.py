e85a42a716df8233da9bf0f4a99e409cf433fe2f
mxiyuan <mxiyuan@outlook.com>
2017-11-24 19:27:36 +0800

    add ch07

---
+++ ch07/adaboost.py
@@ -0,0 +1,10 @@
+def loadSimpData():
+    datMat = matrix([
+        [1.0, 2.1],
+        [1.3, 1.0],
+        [1.0, 1.0],
+        [2.0, 1.0]
+    ])
+    classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+    return datMat, classLabels
+
